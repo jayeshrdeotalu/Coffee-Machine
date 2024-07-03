f90b9a290bf3ecdb29b77babad20a8ce7063bf0e
Jayesh Deotalu <jayesh.deotalu@fluidanalytics.ai>
2024-07-03 23:37:32 +0530

    Created app and main function file

---
+++ app.py
@@ -0,0 +1,11 @@
+# App will be present here ...
+
+'''
+TODOs:
+
+1. Create a app.
+2. Change background, make it a beauty.
+3. add widgets.
+4. Make it userfriendly.
+
+'''
---
+++ coffee_machine.py
@@ -0,0 +1,9 @@
+# Contains code of coffee machine
+
+''' TODOs:
+
+1. Write data, resources, conditions.
+2. Write Code for machine.
+3. Wrtie and test edge condtitions.
+
+'''
